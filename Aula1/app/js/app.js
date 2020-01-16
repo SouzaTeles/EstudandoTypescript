e641bd76636878413228a7913c0c0cb400c0e7c5
lucas teles <lucas@digicriativa.com.br>
2020-01-16 15:15:35 -0300

    Edit: Finalizado Aula 1

--- Aula1/app/js/app.js
+++ Aula1/app/js/app.js
@@ -1,5 +1,2 @@
 const controller = new NegociacaoController();
 $('.form').on('submit', () => controller.adiciona.bind(controller));
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
