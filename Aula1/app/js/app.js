c0d8abd6d47a96bfba505ef8c224aa4d831d74cd
lucas teles <lucas@digicriativa.com.br>
2020-01-16 15:01:36 -0300

    Aula 5: Mudando vanilla.js para jquery

--- Aula1/app/js/app.js
+++ Aula1/app/js/app.js
@@ -1,4 +1,5 @@
 const controller = new NegociacaoController();
+$('.form').on('submit', () => controller.adiciona.bind(controller));
 document
     .querySelector('.form')
     .addEventListener('submit', controller.adiciona.bind(controller));
