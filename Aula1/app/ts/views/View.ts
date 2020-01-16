cc44be649a9b179ced9a47dc5d928bf1e4b87c0b
lucas teles <lucas@digicriativa.com.br>
2020-01-16 19:03:41 -0300

    Escape Script View - Parametros Opicionais

--- Aula1/app/ts/views/View.ts
+++ Aula1/app/ts/views/View.ts
@@ -1,13 +1,17 @@
 export abstract class View<T> { //tipo generico
     private _elemento: JQuery;
+    private _escapar: boolean;
 
-    constructor(selector: string) {
+    constructor(selector: string, escapar?: boolean) {
 
         this._elemento = $(selector);
     }
 
     update(model: T) {
-        this._elemento.html(this.template(model));
+        let template = this.template(model);
+        if(this._escapar)
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '')
+        this._elemento.html(template);
     }
 
     abstract template(model: T): string;
